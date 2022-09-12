ffd8cf518ec9f22c9b578428b9fab65fb6e0fd87
Francisco <francisco@skylan.com.br>
2022-09-12 10:39:17 -0300

    rails/anotacoes/db__seeds.sh: Melhorando explicação sobre o arquivo seeds.rb

--- rails/anotacoes/db__seeds.sh
+++ rails/anotacoes/db__seeds.sh
@@ -1,5 +1,7 @@
+# No seeds.rb existe para ser inseridos os dados padrões do DB da aplicação 
+
 < Model.create!() > # Adiciona os dados na tabela, porém ele não faz a verificação se os dados inseridos já existem na tabela ou não!
 
-< Model.find_or_create_by!() > # Esse também adiciona, porém ele faz a virificação, se o dado não existir, ele o insere, se não, não.
+< Model.find_or_create_by!() > # Esse também adiciona, porém ele faz a virificação, se o dado não existir, ele o insere, se existir, não.
 
 
