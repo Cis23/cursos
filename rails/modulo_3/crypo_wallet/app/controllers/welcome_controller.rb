06a66cb0e22f7ada1030d0ec15a00d8c4c1050db
Francisco <francisco@skylan.com.br>
2022-10-10 14:58:52 -0300

    ...

--- rails/modulo_3/crypo_wallet/app/controllers/welcome_controller.rb
+++ rails/modulo_3/crypo_wallet/app/controllers/welcome_controller.rb
@@ -1,6 +1,9 @@
 class WelcomeController < ApplicationController
   def index
-    @meu_nome = params[:nome];
+    @meu_nome = params[:nome]; 
     @curso = params[:curso];
+    cookies[:curso] = "Curso de Ruby on Rails - Jackson Pires [COOKIE]"
+    session[:curso] = "Curso de Ruby on Rails - Jackson Pires [SESSION]"
   end
+
 end
